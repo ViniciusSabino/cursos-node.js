27db32a2e07f47c7ac8598c82c96ac952f65de92
Vinícius Rocha <vnc.chelsea@gmail.com>
2019-07-09 17:32:27 -0300

    [bootcamp-rocketseat] feat: finalizando 'iniciando back-end do GoBarber'

--- bootcamp2019/modulo02/gobarber/src/routes.js
+++ bootcamp2019/modulo02/gobarber/src/routes.js
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import AuthMiddleware from './middlewares/auth';
+import UserController from './controllers/UserController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hellow World" });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(AuthMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
